8c0f4298dfbeee3f3f546461b57ce6155d3f7b33
USRVI-LC4 <USRVI-LC4@duoc.acad>
2025-09-02 22:20:11 -0400

    add: js y formulario

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-alert("El sitio ha sido completamente cargado y analizado");
+/*alert("El sitio ha sido completamente cargado y analizado");
 
 let nombre = prompt("¿Cuál es tu nombre?");
 alert("Hola " + nombre + ", bienvenido a mi sitio web");
@@ -11,4 +11,28 @@ if (hora < 12) {
     alert("Buenas tardes, " + nombre + "!");
 } else {
     alert("Buenas noches, " + nombre + "!");
+}
+*/
+
+function cambiarTitulo() {
+    let titulo = document.getElementById("titulo");
+
+    if(titulo.classList.contains("rojo")) {
+        titulo.classList.remove("rojo");
+    } else {
+        titulo.classList.add("rojo");
+    }
+}
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre");
+    if (nombre.value.trim() == "" || nombre.value.lenght > 100) {
+        document.getElementById("mensajeErrorNombre").style.display = 'block';
+    } else {
+        document.getElementById("mensajeErrorNombre").style.display = 'none';
+        document.getElementById("formulario").style.display = 'none';
+        document.getElementById("mensajeExito").style.display = 'flex';
+    }
 }
